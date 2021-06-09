938e38300edb9219a90e5ac9c8a1b01510ee6a55
Rebecca Souza <rebeccacsouza@gmail.com>
2021-06-09 02:48:41 +0000

    Automatically backed up by Learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,7 @@
 # write your method here
 def crazy_strings(string1, string2)
    string1 = string1.upcase.reverse
-   string2 = string2.swapcase
+   string2 = string2.swapcase.gsub('s', 'z')
    return string1 + ' ' + string2
 end
 
