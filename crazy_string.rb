8933464e5992e7168d2f59725eb5379c24bf6600
Rebecca Souza <rebeccacsouza@gmail.com>
2021-06-09 02:47:31 +0000

    Automatically backed up by Learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
-
-def crazy_strings(a,b)
-
+def crazy_strings(string1, string2)
+   string1 = string1.upcase.reverse
+   string2 = string2.swapcase
+   return string1 + ' ' + string2
 end
+
+
+#puts crazy_strings("Hello","Friends")
